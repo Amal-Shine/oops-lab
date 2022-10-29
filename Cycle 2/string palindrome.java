78d355214c646058a3561e6dd9fa0a89c7510f9a
Amal-Shine <116619469+Amal-Shine@users.noreply.github.com>
2022-10-29 12:52:16 +0530

    Update string palindrome.java

--- Cycle 2/string palindrome.java
+++ Cycle 2/string palindrome.java
@@ -1,51 +1,32 @@
-import java.util.*;
 import java.util.Scanner;
-public class palindromstring {
- 
-    // Method
-    // Returning true if string is palindrome
-    static boolean isPalindrome(String str)
-    {
- 
-        // Pointers pointing to the beginning
-        // and the end of the string
-        int i = 0, j = str.length() - 1;
- 
-        // While there are characters to compare
-        while (i < j) {
- 
-            // If there is a mismatch
-            if (str.charAt(i) != str.charAt(j))
-                return false;
- 
-            // Increment first pointer and
-            // decrement the other
-            i++;
-            j--;
+class Main  
+{  
+     public static void main(String a[]) 
+     {  
+        Scanner sc=new Scanner(System.in); 
+        System.out.println("Enter the  string: "); 
+        String input = sc.nextLine();
+        if(isPalindrome(input))
+        {
+            System.out.println(input+"is a palindrome string");
         }
- 
-        // Given string is a palindrome
-        return true;
-    }
- 
-    // Method 2
-    // main driver method
-    public static void main(String[] args)
-    {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the string you want");
-        String str = sc.nextLine();
- 
-        //Convert the string to lowercase
-        str = str.toLowerCase();
-        // passing bool function till holding true
-        if (isPalindrome(str))
- 
-            // It is a palindrome
-            System.out.print("Yes its an palindrome");
         else
- 
-            // Not a palindrome
-            System.out.print("its not an palindrome");
-    }
+        {
+            System.out.println(input+"is not a palindrome String");
+        }
+     }
+     public static boolean isPalindrome(String Str)
+     {
+         int left=0,right=Str.length()-1;
+         while(left<right)
+         {
+             if(Str.charAt(left)!=Str.charAt(right))
+             {
+                 return false;
+             }
+             left++;
+             right--;
+         }
+         return true;
+     }
 }
